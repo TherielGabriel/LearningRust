a7f31f6e8190c86f67f7ccd89889846c1095a623
TherielGabriel <rosasalgabriel@gmail.com>
2023-03-25 14:00:31 -0700

    bfs working

--- pathfinding/src/graph.rs
+++ pathfinding/src/graph.rs
@@ -1,12 +1,17 @@
+use crossterm::{cursor, terminal, QueueableCommand};
 use rand::Rng;
-use queue::Queue;
-use crossterm::{QueueableCommand, cursor, terminal, ExecutableCommand};
+use std::collections::{HashMap, VecDeque};
+use std::io::{stdout, Stdout, Write};
 use std::{thread, time};
-use std::io::{Write, stdout};
 
 #[derive(Clone, PartialEq)]
 enum Tile {
-    Start, End, Wall, Searched, Empty
+    Start,
+    End,
+    Wall,
+    Searched,
+    Path,
+    Empty,
 }
 
 impl std::fmt::Display for Tile {
@@ -16,27 +21,28 @@ impl std::fmt::Display for Tile {
             Tile::End => write!(f, "E "),
             Tile::Wall => write!(f, "# "),
             Tile::Searched => write!(f, ". "),
-            Tile::Empty => write!(f, "  "),            
+            Tile::Path => write!(f, "X "),
+            Tile::Empty => write!(f, "  "),
         }
     }
 }
 
 pub struct Graph {
     graph: Vec<Vec<Tile>>,
+    stdout: Stdout,
 }
 
 impl Graph {
-    // While rust doesn't have any explicit constructors, best practice is 
+    // While rust doesn't have any explicit constructors, best practice is
     //  to create a function called "new" that returns an object of the class
     pub fn new() -> Self {
-        let graph = Vec::new();
-        Self { graph }
+        Self::from(11, 11)
     }
 
-    pub fn build(&mut self, width: usize, height: usize) {
-        self.graph = Vec::new();
+    pub fn from(width: usize, height: usize) -> Self {
+        let mut graph = Vec::new();
         for y in 0..height {
-            self.graph.push(vec![Tile::Empty; width]);
+            graph.push(vec![Tile::Empty; width]);
             for x in 0..width {
                 let mut tile = Tile::Empty;
                 // Outer Walls
@@ -51,58 +57,103 @@ impl Graph {
                         tile = Tile::Wall;
                     }
                 }
-                self.graph[y][x] = tile;
+                graph[y][x] = tile;
             }
         }
         let mut rng = rand::thread_rng();
-        self.graph[0][rng.gen_range(0..width/2) * 2 + 1] = Tile::Start;
-        self.graph[height-1][rng.gen_range(0..width/2) * 2 + 1] = Tile::End;
+        graph[0][rng.gen_range(0..width / 2) * 2 + 1] = Tile::Start;
+        graph[height - 1][rng.gen_range(0..width / 2) * 2 + 1] = Tile::End;
+        let stdout = stdout();
+        Self { graph, stdout }
     }
 
-    pub fn run_bfs(&mut self) {
-        let mut stdout = stdout();
-
-        let mut frontier: Queue<(usize, usize)> = Queue::new();
-        for (i, tile) in self.graph[0].iter().enumerate() {
-            if *tile == Tile::Start {
-                frontier.queue((0, i.try_into().unwrap())).unwrap();
-            }
-        }
+    pub fn run_bfs(
+        &mut self,
+    ) -> Result<HashMap<(usize, usize), (usize, usize)>, Box<dyn std::error::Error>> {
+        // HashMap <Destination, Source>
+        let mut paths: HashMap<(usize, usize), (usize, usize)> = HashMap::new();
+        // Frontier: (y, x)
+        let mut frontier: VecDeque<(usize, usize)> = VecDeque::new();
+        let start: (usize, usize) = self.get_start_end().0;
+        paths.insert((start.0 + 1, start.1), start);
+        frontier.push_back((start.0 + 1, start.1));
         while !frontier.is_empty() {
-            let target = frontier.dequeue().unwrap();
+            let target = frontier.pop_front().unwrap();
             self.graph[target.0][target.1] = Tile::Searched;
-
-            stdout.execute(cursor::MoveTo(0, 1)).unwrap();
-            stdout.execute(terminal::Clear(terminal::ClearType::FromCursorDown)).unwrap();
-            stdout.write_all(format!("{}", self.as_str()).as_bytes()).unwrap();
-            stdout.flush().unwrap();
-            thread::sleep(time::Duration::from_millis(100));
-
-            for neighbor in self.neighbors_of(target).iter() {
+            // Drawing graph on top of previous output
+            self.stdout.queue(cursor::MoveTo(0, 4))?;
+            self.stdout
+                .queue(terminal::Clear(terminal::ClearType::FromCursorDown))?;
+            self.stdout
+                .write_all(format!("{}", self.as_str()).as_bytes())?;
+            self.stdout.flush().unwrap();
+            thread::sleep(time::Duration::from_millis(5));
+            for neighbor in self.neighbors_of(target) {
                 if self.graph[neighbor.0][neighbor.1] == Tile::Empty {
-                    frontier.queue(*neighbor).unwrap();
+                    frontier.push_back(neighbor);
+                    paths.insert(neighbor, target);
                 } else if self.graph[neighbor.0][neighbor.1] == Tile::End {
-                    return;
+                    paths.insert(neighbor, target);
+                    return Ok(paths);
                 }
             }
         }
+        Err("Path not found".into())
     }
 
-    fn neighbors_of(&self, target: (usize, usize)) -> Vec<(usize, usize)> {
-        let mut neighbors: Vec<(usize, usize)> = Vec::new();
+    fn neighbors_of(&self, (y, x): (usize, usize)) -> impl Iterator<Item = (usize, usize)> + '_ {
         let (h, w) = (self.graph.len(), self.graph[0].len());
-        match target {
-            (a, b) if (0..h).contains(&(&a + 1)) && (0..w).contains(&b) 
-                && self.graph[a + 1][b] == Tile::Empty => neighbors.push((target.0 + 1, target.1)),
-            (a, b) if (0..h).contains(&(&a - 1)) && (0..w).contains(&b)
-                && self.graph[a - 1][b] == Tile::Empty => neighbors.push((target.0 - 1, target.1)),
-            (a, b) if (0..h).contains(&a) && (0..w).contains(&(&b + 1))
-                && self.graph[a][b + 1] == Tile::Empty => neighbors.push((target.0, target.1 + 1)),
-            (a, b) if (0..h).contains(&a) && (0..w).contains(&(&b - 1))
-                && self.graph[a][b - 1] == Tile::Empty => neighbors.push((target.0, target.1 - 1)),
-            _ => ()
+        let neighbors = [
+            (y + 1, x),
+            (y.wrapping_sub(1), x),
+            (y, x + 1),
+            (y, x.wrapping_sub(1)),
+        ];
+        neighbors.into_iter().filter(move |(i, j)| {
+            (0..w).contains(j)
+                && (0..h).contains(i)
+                && matches!(self.graph[*i][*j], Tile::Empty | Tile::End)
+        })
+    }
+
+    pub fn get_start_end(&self) -> ((usize, usize), (usize, usize)) {
+        let mut start = (0, 0);
+        for (i, tile) in self.graph[0].iter().enumerate() {
+            if *tile == Tile::Start {
+                start = (0, i.try_into().unwrap());
+                break;
+            }
         }
-        neighbors
+        let mut end = (0, 0);
+        for (i, tile) in self.graph[self.graph.len() - 1].iter().enumerate() {
+            if *tile == Tile::End {
+                end = (self.graph.len() - 1, i.try_into().unwrap());
+                break;
+            }
+        }
+        (start, end)
+    }
+
+    pub fn print_path(
+        &mut self,
+        paths: HashMap<(usize, usize), (usize, usize)>,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let (start, end) = self.get_start_end();
+        let mut current = end;
+        while current != start {
+            self.graph[current.0][current.1] = Tile::Path;
+            current = *paths.get(&current).unwrap();
+
+            // Drawing graph on top of previous output
+            self.stdout.queue(cursor::MoveTo(0, 4))?;
+            self.stdout
+                .queue(terminal::Clear(terminal::ClearType::FromCursorDown))?;
+            self.stdout
+                .write_all(format!("{}", self.as_str()).as_bytes())?;
+            self.stdout.flush().unwrap();
+            thread::sleep(time::Duration::from_millis(100));
+        }
+        Ok(())
     }
 
     pub fn as_str(&self) -> String {
--- pathfinding/src/main.rs
+++ pathfinding/src/main.rs
@@ -1,40 +1,18 @@
 // https://stackoverflow.com/questions/59890270/how-do-i-overwrite-console-output
 mod graph;
 use graph::Graph;
-
-use std::{thread, time};
-use std::io::{Write, stdout};
-use crossterm::{QueueableCommand, cursor, terminal, ExecutableCommand};
+use std::collections::HashMap;
 
 fn main() {
+    let (width, height) = (51, 11);
+    let mut graph = Graph::from(width, height);
 
-    let (width, height) = (21, 21);
+    graph.get_start_end();
 
-    let mut graph = Graph::new();
-    graph.build(21, 21);
-
-    graph.run_bfs();
-    // // println!("{}", graph.as_str());
-
-    // let mut stdout = stdout();
-
-    // // stdout.execute(cursor::Hide).unwrap();
-    // println!("Running algoritm");
-    // // stdout.execute(cursor::MoveTo(0, 1)).unwrap();
-    // stdout.execute(cursor::SavePosition).unwrap();
-    // for i in (1..30).rev() {
-    //     //stdout.execute(cursor::RestorePosition).unwrap();
-    //     // stdout.execute(cursor::RestorePosition).unwrap();
-    //     stdout.execute(cursor::MoveTo(0, 1)).unwrap();
-    //     stdout.execute(terminal::Clear(terminal::ClearType::FromCursorDown)).unwrap();
-    //     // stdout.flush().unwrap();
-
-    //     graph.build(21, 21);
-    //     stdout.write_all(format!("{}", graph.as_str()).as_bytes()).unwrap();
-    //     stdout.flush().unwrap();
-
-    //     thread::sleep(time::Duration::from_millis(100));
-    // }
-    // // stdout.execute(cursor::Show).unwrap();
-
+    let paths = graph.run_bfs().unwrap_or(HashMap::new());
+    if paths.len() == 0 {
+        println!("No path was found!");
+        return;
+    }
+    graph.print_path(paths).unwrap();
 }
